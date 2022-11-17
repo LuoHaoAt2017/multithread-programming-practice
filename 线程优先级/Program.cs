55fa88b65960338f3e09abd640ea077bde44ef56
洛神赋 <hluo5206@gmail.com>
2022-11-17 17:11:35 +0800

    线程的优先级

---
+++ 线程优先级/Program.cs
@@ -0,0 +1,37 @@
+﻿
+public class Program
+{
+	public static void Main()
+	{
+		List<Thread> threads = new List<Thread>();
+
+		List<ThreadPriority> priorityList = new List<ThreadPriority>() { 
+			ThreadPriority.Lowest, 
+			ThreadPriority.BelowNormal, 
+			ThreadPriority.Normal,
+			ThreadPriority.AboveNormal,
+			ThreadPriority.Highest
+		};
+
+		for(int i = 0; i < priorityList.Count; i++)
+		{
+			int index = i;
+			Thread thread = new Thread(() =>
+			{
+				Thread.Sleep(1000);
+				Console.WriteLine($"Thread-{index}");
+			});
+			thread.Priority = priorityList[index];
+			threads.Add(thread);
+		}
+
+		for(int i = 0; i < threads.Count; i++)
+		{
+			Thread thread = threads[i];
+			thread.Start();
+		}
+		
+		Console.WriteLine("优先级高的线程一定先执行，然后才会轮到优先级低的线程执行。这种认识是错误的。");
+		Console.WriteLine("优先级高的线程相较于优先级低的线程有较大的概率优先执行，是概率层面。");
+	}
+}
